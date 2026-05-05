32349dc80f8e6bbc5a523f02085c6f81d6d5acc7
Dark <kilometro0ec@gmail.com>
2026-05-05 09:10:44 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'napoles-v38'; // CAMBIA versión SIEMPRE
+const CACHE_NAME = 'napoles-v39'; // CAMBIA versión SIEMPRE
 
 self.addEventListener('install', e=>{
   self.skipWaiting();
