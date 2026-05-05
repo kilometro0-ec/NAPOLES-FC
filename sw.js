6986ac3626b66cae20f182b39cc40c9da2f8e06a
Dark <kilometro0ec@gmail.com>
2026-05-05 03:28:52 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,18 @@
-const CACHE_NAME = 'napoles-dynamic-v1';
+const CACHE_NAME = 'napoles-v10';
 
-const urlsToCache = [
-  './',
-  './index.html',
-  './estilos.css',
-  './api.js',
-  './nav.js'
-];
-
-// instalar
 self.addEventListener('install', e=>{
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))
-  );
 });
 
-// activar y limpiar
 self.addEventListener('activate', e=>{
   e.waitUntil(
     caches.keys().then(keys=>{
-      return Promise.all(
-        keys.map(k=>{
-          if(k !== CACHE_NAME) return caches.delete(k);
-        })
-      );
+      return Promise.all(keys.map(k=>caches.delete(k)));
     })
   );
 });
 
-// 🔥 NETWORK FIRST SIEMPRE
+// 🚀 SIEMPRE RED (NUNCA CACHE VIEJO)
 self.addEventListener('fetch', e=>{
-  e.respondWith(
-    fetch(e.request)
-      .then(res=>{
-        const copy = res.clone();
-        caches.open(CACHE_NAME).then(cache=>cache.put(e.request, copy));
-        return res;
-      })
-      .catch(()=>caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
