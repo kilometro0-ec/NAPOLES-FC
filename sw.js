0d91d55f82aa1d3b234f37142b7f184763410fb4
Dark <kilometro0ec@gmail.com>
2026-05-05 04:12:44 -0500

    Change CACHE_NAME to 'napoles-v9'
    Updated cache version to v9.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'napoles-v8'; // CAMBIA versión SIEMPRE
+const CACHE_NAME = 'napoles-v9'; // CAMBIA versión SIEMPRE
 
 self.addEventListener('install', e=>{
   self.skipWaiting();
