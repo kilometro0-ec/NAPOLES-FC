4003e104ec8dd3ac6da25cf2306ab7456e303766
Dark <kilometro0ec@gmail.com>
2026-05-05 18:19:56 -0500

    Change cache name to 'napoles-v2'
    Updated cache version to v2 for service worker.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'napoles-v1'; // CAMBIA versión SIEMPRE
+const CACHE_NAME = 'napoles-v2'; // CAMBIA versión SIEMPRE
 
 self.addEventListener('install', e=>{
   self.skipWaiting();
