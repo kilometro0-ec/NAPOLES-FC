04301b349ae5c1098f57a80eddaa51d7732a570e
Dark <kilometro0ec@gmail.com>
2026-05-05 07:28:46 -0500

    Update CACHE_NAME to version v32

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'napoles-v31'; // CAMBIA versión SIEMPRE
+const CACHE_NAME = 'napoles-v32'; // CAMBIA versión SIEMPRE
 
 self.addEventListener('install', e=>{
   self.skipWaiting();
