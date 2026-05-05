3b880b1ff3977a0e3d9980d62899f04e6e502da0
Dark <kilometro0ec@gmail.com>
2026-05-05 08:36:40 -0500

    Increment cache version to 'napoles-v36'
    Updated cache version to ensure new assets are cached.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'napoles-v35'; // CAMBIA versión SIEMPRE
+const CACHE_NAME = 'napoles-v36'; // CAMBIA versión SIEMPRE
 
 self.addEventListener('install', e=>{
   self.skipWaiting();
