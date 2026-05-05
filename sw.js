0da6e82fec94f07035806862082b284934bc643d
Dark <kilometro0ec@gmail.com>
2026-05-05 04:02:39 -0500

    Update cache version to 'napoles-v7'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'napoles-v6'; // CAMBIA versión SIEMPRE
+const CACHE_NAME = 'napoles-v7'; // CAMBIA versión SIEMPRE
 
 self.addEventListener('install', e=>{
   self.skipWaiting();
