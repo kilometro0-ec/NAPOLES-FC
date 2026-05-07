4009336d59ae4a5ec8587eae9b1b5102152d4ede
Dark <kilometro0ec@gmail.com>
2026-05-07 02:34:57 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    URL_GAS: 'https://script.google.com/macros/s/AKfycby2Dy9Uh1YV2rRN7dzMnnxBBqIQ5RV4LMQ0Hkre08rBWdA9Lky-cJWjTHsgjwx1F7j7/exec',  // ✅ Cambia por tu URL real
+    URL_GAS: 'https://script.google.com/macros/s/AKfycby_YKcfqtDEsWzl5MJui_KBaHboyTFL6CPoE2uucLtzBza0vQ6MljiP9hc6fnkQumy1/exec',  // ✅ Cambia por tu URL real
     APP_NAME: 'Nápoles F.C.',
     VERSION: '1.0.0',
     TIEMPO_SESION: 86400000,
