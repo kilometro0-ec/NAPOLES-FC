0a3c385afb88c1a93a07d425d89c660ec4057cce
Dark <kilometro0ec@gmail.com>
2026-05-07 00:10:50 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// config.js - Configuración centralizada
+const CONFIG = {
+    // URL del Google Apps Script (cámbiala SOLO aquí)
+    URL_GAS: 'https://script.google.com/macros/s/AKfycbw93aokkZs0TTqSHv5LgJsBG3yLN2eOEmcwcvjNFzbtOnUeCCtMx5lQP4mq8loPC9Ky/exec',
+    
+    // Otras configuraciones globales
+    APP_NAME: 'Nápoles F.C.',
+    VERSION: '1.0.0',
+    TIEMPO_SESION: 86400000, // 24 horas en milisegundos
+    
+    // URLs de assets
+    DEFAULT_AVATAR: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
+    APP_ICON: 'https://cdn-icons-png.flaticon.com/512/1165/1165187.png'
+};
