09d0533dbf45a7c442cda362fe314439cb1a576b
Dark <kilometro0ec@gmail.com>
2026-05-09 02:00:29 -0500

    Update URL and default avatar in config.js

--- config.js
+++ config.js
@@ -1,12 +1,11 @@
 const CONFIG = {
 
-    // ⚠️ PEGA AQUÍ TU URL REAL DE APPS SCRIPT
-    URL_GAS: 'TU_URL_WEBAPP_APPS_SCRIPT',
+    // PEGA TU URL NUEVA DEL APPS SCRIPT
+    URL_GAS: 'TU_URL_WEBAPP',
 
-    // Avatar por defecto
-    DEFAULT_AVATAR: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+    DEFAULT_AVATAR:
+    'https://cdn-icons-png.flaticon.com/512/149/149071.png',
 
-    // 24 horas
     TIEMPO_SESION: 86400000
 
 };
