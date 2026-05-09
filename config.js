ef26c7a8980eb51d2f61151dec26b59e57013203
Dark <kilometro0ec@gmail.com>
2026-05-09 02:02:00 -0500

    Update URL_GAS to new Google Apps Script URL

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
 
     // PEGA TU URL NUEVA DEL APPS SCRIPT
-    URL_GAS: 'TU_URL_WEBAPP',
+    URL_GAS: 'https://script.google.com/macros/s/AKfycbyf26moa1M1f4Bch74mg7LX4kgt7MSNowHw0BEhwUE5QDo9yp8sQWnRTYdmcpwV4itx/exec',
 
     DEFAULT_AVATAR:
     'https://cdn-icons-png.flaticon.com/512/149/149071.png',
