22e7136dc777e975488b81ce7036470344b07b80
Dark <kilometro0ec@gmail.com>
2026-05-09 11:27:53 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
 
     // PEGA TU URL NUEVA DEL APPS SCRIPT
-    URL_GAS: 'https://script.google.com/macros/s/AKfycbyD76mwo_MOAhoA5c_58lxBeCinwDOzSc0aiS0Tln44w4kQBHJm4BYYzjc0AkA33GAw/exec',
+    URL_GAS: 'https://script.google.com/macros/s/AKfycbzpL71kEp6oZa3uG0hI5gdoGK6864J8LqR4o-p4ZoiA3gqkwX2-V8qX7dCIvJ_bALtV/exec',
 
     DEFAULT_AVATAR:
     'https://cdn-icons-png.flaticon.com/512/149/149071.png',
