18e284abb84322c8c6104bdfb2e5285417bc9272
Dark <kilometro0ec@gmail.com>
2026-05-11 23:53:43 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    URL_GAS: 'https://script.google.com/macros/s/AKfycbwVlnbTNb1KgpDcIBLmo1WMHoDMk_MAefvqES_Kp8DuTeO4uE4V0KS35AX9sOsGlaW5/exec',
+    URL_GAS: 'https://script.google.com/macros/s/AKfycbxxtrgdP2LTYERUxwsZamImluU5skk3i9fAAMBxoEFJCDpc2aCcF8z9g7bLa1dHE2lf/exec',
     DEFAULT_AVATAR: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
     TIEMPO_SESION: 86400000
 };
