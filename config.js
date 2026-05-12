96502b5b51dd36d2bbcf52f430f9d2fb7220ed6d
Dark <kilometro0ec@gmail.com>
2026-05-12 08:33:23 -0500

    Update GAS URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    URL_GAS: 'https://script.google.com/macros/s/AKfycbx4OK8l3NCxzVGi83RzTvdFSSem9VyCGfdE6NzhbOTvQialzxOvdDggkPjXVX9DY-YC/exec',
+    URL_GAS: 'https://script.google.com/macros/s/AKfycbypBFmmeT7o5YivVI84JwFQOzc5bKokS0J8sVIPCH2n9n35aGgUCEe1p7zDk0pDK452/exec',
     DEFAULT_AVATAR: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
     TIEMPO_SESION: 86400000
 };
