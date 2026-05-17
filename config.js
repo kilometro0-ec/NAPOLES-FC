48928ab74b6deb5d644838b6cb1a8f3bfc763bb0
Dark <kilometro0ec@gmail.com>
2026-05-16 19:28:30 -0500

    Update URL_GAS to new Google Apps Script URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // ASEGÚRATE DE QUE LA URL TERMINE EN /exec
-    URL_GAS: "https://script.google.com/macros/s/AKfycbwCFIyUaJtW0AS22VnyB8fKpy90KSRBHcREGkJN-jSDWe5kcuVLYmvbg8w4UOnEgJmk/exec"
+    URL_GAS: "https://script.google.com/macros/s/AKfycbyLefSqOF9st7FqQU3o_qY5xL2dv5d1bWFpIJUN8je7SPDhX6wFMdlxddNdgQ0IRNM/exec"
 };
 
 // ESTO ES PARA PROBAR SI EL ARCHIVO CONFIG.JS SE CARGA BIEN
