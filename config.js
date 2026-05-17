24a33a06e580502bdd472d6dcc849a68fd18da1d
Dark <kilometro0ec@gmail.com>
2026-05-16 23:34:28 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // ASEGÚRATE DE QUE LA URL TERMINE EN /exec
-    URL_GAS: "https://script.google.com/macros/s/AKfycbwhBmsgEhwJt-9PBM2dx2krYIl4nEs7MiNyHjT53NE9bQWF48cN5SjqutqBehPhKA/exec"
+    URL_GAS: "https://script.google.com/macros/s/AKfycbz0-Nyxxx0XWKzBlEe5XpTvBP5RVVsmaEkhcG8Jn7QwAT65mvdV7Mw303jDsLoKKTl2/exec"
 };
 
 // ESTO ES PARA PROBAR SI EL ARCHIVO CONFIG.JS SE CARGA BIEN
