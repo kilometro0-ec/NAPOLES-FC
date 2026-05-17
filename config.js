22f5a5889303c9ed69123126950b5665e1c399ab
Dark <kilometro0ec@gmail.com>
2026-05-16 20:54:25 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 
 const CONFIG = {
-    // ASEGÚRATE DE QUE LA URL TERMINE EN /exec
+    // URL de Google Apps Script actualizada
     URL_GAS: "https://script.google.com/macros/s/AKfycbz47dM-zlZDGxaPkb8mKj1Gyp4an96qOfSQI2uurtjOlukmFXMInZXnX5lYYsLeU1Dz/exec"
 };
 
-// ESTO ES PARA PROBAR SI EL ARCHIVO CONFIG.JS SE CARGA BIEN
-console.log("Configuración cargada correctamente. URL:", CONFIG.URL_GAS); funionalll
+// Test de verificación en consola
+console.log("Configuración cargada correctamente. URL:", CONFIG.URL_GAS);
